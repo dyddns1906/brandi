ed43d64dd116f0bdd9a9084a0a9ab2995d06c850
Yongun Lim <dyddns1906@gmail.com>
2021-07-29 10:05:55 +0900

    Initial Commit

---
+++ brandi/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  brandi
+//
+//  Created by Yongun Lim on 2021/07/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
